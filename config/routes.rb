e2c35605eb2cd90d011b09db197c41d04557c56e
Anders Drew & Matthew LeFurge <pair+cernalanders.mlefurge@devbootcamp.com>
2015-11-09 10:42:20 -0500

    adds routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'categories#index'
+  resources :categories do
+    resources :topics
+  end
+  resources :conversations do
+    resources :messages
+  end
+  resources :users do
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
