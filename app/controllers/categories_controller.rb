5ec5a76c5055e7ebb0003d579663d1689c58d4e3
Anders Drew & Matthew LeFurge <pair+cernalanders.mlefurge@devbootcamp.com>
2015-11-09 11:21:20 -0500

    adds category and topics list links

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,11 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @topics = Topic.where(category_id: @category.id)
   end
 
   def new
