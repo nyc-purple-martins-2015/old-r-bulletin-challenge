55b1a8b92dc3b41e58629f62faa37f6b2fc7edea
Nick Perri & Rebecca Kleinberg <pair+nperri6.rebeccakleinberg@devbootcamp.com>
2015-11-09 11:51:52 -0500

    Release 0 complete with ordered by conversation date

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -10,5 +10,6 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
     @conversations = @topic.conversations
+    @conversations = @conversations.sort {|a,b| b.updated_at <=> a.updated_at}
   end
 end
