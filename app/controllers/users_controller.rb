5670a53d9c81777027798eb124aa2b4830d4d917
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-09 18:52:05 -0500

    added user controller error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path
     else
+      flash.now[:notice] = "Blank or Invalid User Information"
       render :new
     end
   end
