01b5f914cee73e1508586763ba086dce5120c390
Angelita Graves & Mae Capozzi <pair+gravesal.maecapozzi@devbootcamp.com>
2015-11-09 16:54:43 -0500

    add ability to create user

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     resources :messages
   end
 
+  resource :session, only: [:create, :new, :destroy]
+  resources :users, only: [:create, :new]
+
+
+  # get 'register' :'users#new'
+
   root 'topics#index'
 end
 
