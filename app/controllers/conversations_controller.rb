04c7069185c85c75e5cb2e698efe2c17ecce1fb0
Steven G. Harms <steven@devbootcamp.com>
2015-11-10 12:10:22 -0500

    R0: As a visitor I want to see a list of recently active conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,4 +2,9 @@ class ConversationsController < ApplicationController
   def show
     @conversation = Conversation.includes(:messages).find(params[:id])
   end
+
+  def index
+    @topic = Topic.find(params[:topic_id])
+    @conversations = @topic.recent_conversations
+  end
 end
