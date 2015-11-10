aa782a6a72a939879602c2167e9e3d859cf63bf3
Steven G. Harms <steven@devbootcamp.com>
2015-11-10 16:56:54 -0500

    Messages#create: Better UX, redir back to conversation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,12 +1,13 @@
 class MessagesController < ApplicationController
   def create
-    message = Conversation.find(params[:conversation_id]).messages.build(message_params.merge(user_id: current_user.id))
+    conversation = Conversation.find(params[:conversation_id])
+    message = conversation.messages.build(message_params.merge(user_id: current_user.id))
     if message.save
       flash[:notice] = "Message created"
     else
       flash[:alert] = "Error creating message"
     end
-    redirect_to topics_path
+    redirect_to [conversation.topic, conversation]
   end
 
   private
