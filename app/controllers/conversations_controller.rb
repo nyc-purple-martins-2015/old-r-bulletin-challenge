45d967c308abbea87a66e138b989f9262201874d
Tahrim Uddin & Alicia <pair+utahrim.yokuba@devbootcamp.com>
2015-11-09 20:16:10 -0500

    Add message form/create

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,5 +2,7 @@ class ConversationsController < ApplicationController
 
 	def show
 		@conversation = Conversation.find(params[:id])
+		@message = Message.new
+		@message.conversation = @conversation
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   resources :topics do 
     resources :conversations
   end
+  resources :conversations, only: [:show] do 
+    resources :messages, only: [:create]
+  end
+ 
+
   resources :users
   root 'topics#index'
   get 'register' => 'users#new'
