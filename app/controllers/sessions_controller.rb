da62f9e75d7392de3c75cbf6669713f1af6e40a0
Steven G. Harms <steven@devbootcamp.com>
2015-11-10 15:05:30 -0500

    R1: As a user I want to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,20 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     redirect_to topics_path
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    byebug
+    user = User.find_by(username: params[:user][:username])
+    if user.password == params[:password]
+      flash[:notice] = "Awesome sauce"
+      session[:user_id] = user.id
+    else
+      flash[:alert] = "bad password"
+    end
+    redirect_to topics_path
+  end
 end
