563fe4886f63dc6b15be2e0cc1a89beeb724e90f
Jiyoon Kim <jiyoonkim97@gmail.com>
2015-11-09 21:20:18 -0500

    ajaxed with different method

--- app/assets/javascripts/conversations.js
+++ app/assets/javascripts/conversations.js
@@ -1,30 +1,30 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-  $(".new-message-link").on("click", function(event){
-    event.preventDefault();
-    var url = $(this).children().attr("href");
+//   $(".new-message-link").on("click", function(event){
+//     event.preventDefault();
+//     var url = $(this).children().attr("href");
 
-    var request = $.ajax({
-      method: "get",
-      url: url,
-    }).done(function(form){
-      var new_message_form = $(form).children().last();
-      $(".new-message-link").replaceWith(new_message_form)
-    });
-  });
+//     var request = $.ajax({
+//       method: "get",
+//       url: url,
+//     }).done(function(form){
+//       var new_message_form = $(form).children().last();
+//       $(".new-message-link").replaceWith(new_message_form)
+//     });
+//   });
 
-  $(".new-conversation-link").on("click", function(event){
-    event.preventDefault();
-    var url = $(this).children().attr("href");
+//   $(".new-conversation-link").on("click", function(event){
+//     event.preventDefault();
+//     var url = $(this).children().attr("href");
 
-    var request = $.ajax({
-      method: "get",
-      url: url,
-    });
+//     var request = $.ajax({
+//       method: "get",
+//       url: url,
+//     });
 
-    request.done(function(form){
-      var new_conv_form = $(form).children().last();
-      $(".new-conversation-link").replaceWith(new_conv_form)
-    });
-  });
-});
+//     request.done(function(form){
+//       var new_conv_form = $(form).children().last();
+//       $(".new-conversation-link").replaceWith(new_conv_form)
+//     });
+//   });
+// });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,7 @@
 class MessagesController < ApplicationController
+  before_action :all_messages, only: [:create]
+  respond_to :html, :js
+
   def new
     conversation_id = params[:conversation_id]
     @message = Message.new
@@ -11,12 +14,17 @@ class MessagesController < ApplicationController
       redirect_to @conversation
     else
       flash.now[:notice] = "No Message"
+      render :form
       # render layout: false
       # line 14 does not turn off layout, had to pull out with ajax.
     end
   end
 
   private
+
+  def all_messages
+    @messages = Message.all
+  end
 
   def message_params
     params.require(:message).permit(:content).merge(conversation_id: @conversation.id)
