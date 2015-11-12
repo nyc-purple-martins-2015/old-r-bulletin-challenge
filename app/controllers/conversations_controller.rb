c323aff5cafe5f0e62557f938a99cf9bebb9c5a7
Lawson & Alicia <pair+sonomar.yokuba@devbootcamp.com>
2015-11-11 19:01:43 -0500

    Show Page Test for Conversations Controller Complete

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,12 +1,14 @@
 class ConversationsController < ApplicationController
-  def show
-    @conversation = Conversation.includes(:messages).find(params[:id])
-  end
 
   def index
     @topic = Topic.find(params[:topic_id])
     @conversations = @topic.recent_conversations
   end
+
+  def show
+    @conversation = Conversation.includes(:messages).find(params[:id])
+  end
+
 
   def create
     topic = Topic.find(params[:topic_id])
